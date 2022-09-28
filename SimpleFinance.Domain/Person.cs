5f873fd30b536ac053e3045e9707e7f6277dd2e8
Richard Laskowski <richard.lee.laskowski@gmail.com>
2022-09-28 08:36:33 -0500

    Added missing ';'.

--- SimpleFinance.Domain/Person.cs
+++ SimpleFinance.Domain/Person.cs
@@ -4,7 +4,7 @@ namespace SimpleFinance.Domain
 {
 	public abstract class Person : DomainModel
 	{
-		protected int PersonId { get => Id }
+		protected int PersonId { get => Id; }
 		public string FirstName { get; set; }
 		public string LastName { get; set; }
 		public string FullName { get => $"{FirstName} {LastName}"; }
