4d24ec7070952a6584d77efd65f22471ee816fb6
Richard Laskowski <richard.lee.laskowski@gmail.com>
2022-09-28 15:17:33 -0500

    Added Bogus implementation of owners for a test.

--- SimpleFinance.Console/Program.cs
+++ SimpleFinance.Console/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using Bogus;
+using SimpleFinance.Domain;
+
+namespace SimpleFinance.ConsoleUI
+{
+	internal class Program
+	{
+		private const int Count = 100;
+
+		static void Main(string[] args)
+		{
+			int ownerIds = 1;
+
+			Faker<Owner> ownerFaker = new Faker<Owner>()
+			.CustomInstantiator(f => new Owner(ownerIds++, f.Name.FirstName(f.PickRandom(f.Person.Gender)), f.Name.LastName()));
+
+
+			List<Owner> owners = ownerFaker.Generate(Count);
+
+			foreach (Owner owner in owners)
+			{
+				System.Console.WriteLine($"{owner.OwnerId}: {owner.FullName}");
+
+			}
+		}
+	}
+}
